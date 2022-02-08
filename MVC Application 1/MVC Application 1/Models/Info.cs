926544a788de91294836f5a600c79c2439fb1c90
Austin <g42pixels@gmail.com>
2022-02-08 09:18:27 +0200

    Initial Definition of Models
    Primitive Models created
    ToDo: DataAnnotations

---
+++ MVC Application 1/MVC Application 1/Models/Info.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Application_1.Models
+{
+    public class Info
+    {
+    }
+}
