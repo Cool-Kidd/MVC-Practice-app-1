f996798c4599d73399322b6106c8aa3d899e30b6
Austin <g42pixels@gmail.com>
2022-02-08 09:25:32 +0200

    Primary Keys Defined

--- MVC Application 1/MVC Application 1/Models/Info.cs
+++ MVC Application 1/MVC Application 1/Models/Info.cs
@@ -1,11 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MVC_Application_1.Models
 {
     public class Info
     {
+        [Key]
+        [Column(Order = 1)]
+        public int PersonID { get; set; }
+        [Key]
+        [Column(Order = 2)]
+        public int TelNo { get; set; }
+        [Key]
+        [Column(Order = 3)]
+        public int CellNo { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string AddressLine3 { get; set; }
+        public int AddressCode { get; set; }
+        public string PostalAddress1 { get; set; }
+        public string PostalAddress2 { get; set; }
+        public int PostalCode { get; set; }
     }
 }
--- MVC Application 1/MVC Application 1/Models/Person.cs
+++ MVC Application 1/MVC Application 1/Models/Person.cs
@@ -1,11 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVC_Application_1.Models
 {
     public class Person
-    {
+    {   
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Password { get; set; }
+        public DateTime LastLogin { get; set; }
+
     }
 }
